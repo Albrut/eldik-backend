e451a5abc1059e4cf9df433f7c613484c429e32b
Альберт Гадиев <albertgadiev@yandex.com>
2025-04-16 15:21:40 +0600

    added web dependecy

--- src/main/java/kg/eldik/incidentmanagement/controller/rest_api/GayC.java
+++ src/main/java/kg/eldik/incidentmanagement/controller/rest_api/GayC.java
@@ -1,14 +1,17 @@
 package kg.eldik.incidentmanagement.controller.rest_api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GayC {
+    @Value("${spring.datasource.username}")
+    private String da;
 
     @GetMapping("/")
     public String hello() {
-        return "Привет! 👋";
+        return da;
     }
 }
 
