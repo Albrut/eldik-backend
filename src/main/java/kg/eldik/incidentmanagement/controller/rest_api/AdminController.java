6c61084b7443c8823f5d886fc5caae3ad7103c46
osolenye <raiimkulov.bekzat@gmail.com>
2025-04-22 23:39:17 +0600

    check

--- src/main/java/kg/eldik/incidentmanagement/controller/rest_api/AdminController.java
+++ src/main/java/kg/eldik/incidentmanagement/controller/rest_api/AdminController.java
@@ -12,18 +12,13 @@ import kg.eldik.incidentmanagement.service.IncidentRequestService;
 import kg.eldik.incidentmanagement.service.SystemAdminService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "*") // Разрешить любые источники
 @RestController
 @RequestMapping("/api/v1/admin")
 public class AdminController {
